e27df6a8ba24703fcd859b84f5c5255727330a94
Leonardodezi <leonardo.dezi@mail.polimi.it>
2020-03-23 16:53:39 +0100

    Update

--- src/main/java/Model/Board.java
+++ src/main/java/Model/Board.java
@@ -4,9 +4,9 @@ import java.util.ArrayList;
 
 public class Board { //nelle classi ho provato a scrivere le cose messe nell'UML,
     private Square[][] fullMap; //ci sono cose di sintassi che bisogna guardare.
-    private Player ArrayList playerlist; //qui non capisco perchè li segna rossi
+    private ArrayList<Player> playerlist;
 
-    public Board{
+    public Board(){
         fullMap = new Square[6][6];
         for (int i=0; i<6; i++){ //questa è l'inizializzazione della board, bisogna mettere le torri ai lati ma
             for(int j=0; j<6; j++){ //finchè non prende il collegamento con square non so come fare
@@ -14,7 +14,9 @@ public class Board { //nelle classi ho provato a scrivere le cose messe nell'UML
             }
         }
     }
-
+    public Notify(){
+        
+    }
 
 
 
--- src/main/java/Model/Square.java
+++ src/main/java/Model/Square.java
@@ -3,7 +3,6 @@ package Model;
 public class Square {
     private int value;
     private final Coordinates coordinates;
-    private boolean isDome;
     private int level;
 
     public Square(int i, int j) {
@@ -11,11 +10,9 @@ public class Square {
         if(i==6||i==0 || j==6 || j==0){
             this.level = 5;  // il recinto di cupole
             this.value = 3; // 3 per indicare la cupola? TBD
-            this.isDome = true; // questo lo vogliamo togliere alla fine?
         }else{
             this.value = 0;   //ho messo value=0 come casella libera per il momento TBD
             this.level = 0;
-            this.isDome = false;
         }
     }
 
