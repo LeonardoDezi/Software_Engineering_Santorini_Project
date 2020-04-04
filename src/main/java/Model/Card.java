b7bee1d387f9cd430f55e6f9e05d35b25c929ad7
BrMarco99 <marco1.bruni@mail.polimi.it>
2020-04-04 19:23:06 +0200

    Adding classes Card, CardConditions, CardEffects: Trying to repair the synchronizing conflict

--- src/main/java/Model/Card.java
+++ src/main/java/Model/Card.java
@@ -1,4 +1,27 @@
 package Model;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.io.IOException;
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
 public class Card {
+    private String name;
+    private CardConditions conditions;
+    private CardEffects effects;
+
+    public void upload() throws IOException {
+
+        Gson gson = new Gson();
+        Reader reader = Files.newBufferedReader(Paths.get("Cards.json"));
+        List<Card> cards = new Gson().fromJson(reader, new TypeToken<List<Card>>() {}.getType());
+        cards.forEach(System.out::println);
+        reader.close();
+
+    }
+
 }
--- src/main/java/Model/CardEffects.java
+++ src/main/java/Model/CardEffects.java
@@ -1,4 +1,12 @@
 package Model;
 
 public class CardEffects {
+    private String movement;
+    private boolean samesquare;
+    private int building;
+    private String moveOpponent;
+    private String beforeMovement;
+    private boolean win;
+    private boolean dome;
+
 }
