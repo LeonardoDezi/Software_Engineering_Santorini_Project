8d3f8015ee23d2d6ea2ef7532861580fe78c4eb4
leona <leonardo.dezi@mail.polimi.it>
2020-04-19 18:31:08 +0200

    Turn and context implemented

--- src/main/java/it/polimi/ingsw/Model/Context.java
+++ src/main/java/it/polimi/ingsw/Model/Context.java
@@ -1,4 +1,18 @@
 package it.polimi.ingsw.Model;
 
+
 public class Context {
+    private State state;
+
+    public Context(){
+        state = null;
+    }
+
+    public void setState(State state){
+        this.state = state;
+    }
+
+    public State getState(){
+        return state;
+    }
 }
