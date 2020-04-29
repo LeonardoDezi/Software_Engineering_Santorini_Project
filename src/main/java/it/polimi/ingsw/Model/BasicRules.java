c580f38095473e516c2db7d962c7785ba84a9d1f
leona <leonardo.dezi@mail.polimi.it>
2020-04-29 18:05:28 +0200

    First atomization (will fix cards power in cascade)

--- src/main/java/it/polimi/ingsw/Model/BasicRules.java
+++ src/main/java/it/polimi/ingsw/Model/BasicRules.java
@@ -6,13 +6,13 @@ import java.util.ArrayList;
 
 public class BasicRules implements Rules {
     private Card card;
-    private int maxBuild=1;
-    private int numMoves=1;
-    private int maxHeight ;
+    private int maxBuild = 1;
+    private int numMoves = 1;
+    private int maxHeight;
     private Board board;
     private Game game;
 
-    public BasicRules(Board board, Game game){
+    public BasicRules(Board board, Game game) {
         this.board = board;
         this.game = game;
         this.maxHeight = 1;
@@ -28,36 +28,38 @@ public class BasicRules implements Rules {
 
     /**
      * is used to finalize a move.
+     *
      * @param builder is the selected builder that is moving.
-     * @param x is the x coordinate where the builder wants to move.
-     * @param y is the y coordinate where the builder wants to move.
+     * @param x       is the x coordinate where the builder wants to move.
+     * @param y       is the y coordinate where the builder wants to move.
      */
     @Override
-    public void movement(Builder builder, int x, int y){
+    public void movement(Builder builder, int x, int y) {
         winCondition(builder, x, y);
         board.move(builder, x, y);
     }
 
     /**
      * is used to finalize the building of a new level.
+     *
      * @param x is the x coordinate where the player wants to build
      * @param y is the y coordinate where the player wants to build
      */
     @Override
-    public void building(int x, int y){
+    public void building(int x, int y) {
         board.build(x, y, false);
     }
 
-   @Override
-    public void winCondition(Builder builder, int x, int y){
+    @Override
+    public void winCondition(Builder builder, int x, int y) {
         Square actualPosition = builder.getPosition();
         if (actualPosition.getLevel() == 2) {
             if (board.fullMap[x][y].getLevel() == 3) {
                 String colour = builder.getColour();
-                int i=0;
-                while(game.playerList.get(i) != null) {
+                int i = 0;
+                while (game.playerList.get(i) != null) {
                     if (game.playerList.get(i).colour.equals(colour)) {
-                     //   endGame(game.playerList.get(i));   SOLO PER TESTARE
+                        //   endGame(game.playerList.get(i));   SOLO PER TESTARE
                     }
                     i++;
                 }
@@ -67,97 +69,56 @@ public class BasicRules implements Rules {
 
     /**
      * is used to give the player all the available moves.
+     *
      * @param player is the player that can move.
      * @return an ArrayList of all the possible moves of all the the builder of a player.
      */
-    public ArrayList<Square> getMovementRange(Player player){
+    public ArrayList<Square> getMovementRange(Player player) {
         ArrayList<Square> firstPossibleMoves;
         ArrayList<Square> secondPossibleMoves;
 
         firstPossibleMoves = getPossibleMoves(player.builders.get(0));
         try {
             secondPossibleMoves = getPossibleMoves(player.builders.get(1));
-        }catch(NullPointerException e) {
+        } catch (NullPointerException e) {
             secondPossibleMoves = null;
         }
 
 
-        if(firstPossibleMoves.isEmpty() && secondPossibleMoves.isEmpty()){
-           // losecondition();  // ci mettiamo un exception da propagare al chiamante SOLO PER TESTARE
+        if (firstPossibleMoves.isEmpty() && secondPossibleMoves.isEmpty()) {
+            // losecondition();  // ci mettiamo un exception da propagare al chiamante SOLO PER TESTARE
         }
 
         return firstPossibleMoves; //ritornare tutti e 4 gli array
     }
 
+
     @Override
     public ArrayList<Square> getPossibleMoves(Builder builder) {
 
-        ArrayList<Square> possibleMoves = new ArrayList<>();
-        Square position = builder.getPosition();
+        ArrayList<Square> possibleMoves = proximity(builder);
+        possibleMoves = removeBuilderSquare(possibleMoves);
+        possibleMoves = removeDomeSquare(possibleMoves);
+        possibleMoves = removeTooHighPlaces(possibleMoves, builder);
 
-        int i, j;
-
-        for (i = -1; i <= 1; i++) {
-
-            for (j = -1; j <= 1; j++) {
-
-                if (i != 0 || j != 0) {
-
-
-                    int playerHeight = position.getLevel();
-
-                    int a = position.x + i;
-                    int b = position.y + j;
-
-                    if (a >= 0 && a <= 5 && b >= 0 && b <= 5) {
-                        int otherHeight = board.fullMap[a][b].getLevel();
-                        if (playerHeight - otherHeight <= maxHeight) {
-                            int newSquareValue = board.fullMap[a][b].getValue();
-                            if (newSquareValue == 0) {
-                                Square square = board.fullMap[a][b];
-                                possibleMoves.add(square);
-                            }
-                        }
-                    }
-                }
-            }
-        }
 
         return possibleMoves;
     }
 
 
-
     /**
      * is used to give the player all the available places to build.
+     *
      * @param builder is the builder thant can build.
      * @return an arrayList with all the possible places to build.
      */
 
     @Override
-    public ArrayList<Square> getBuildingRange(Builder builder){
+    public ArrayList<Square> getBuildingRange(Builder builder) {
 
-        ArrayList<Square> possibleBuilds = new ArrayList<>();
-        Square position = builder.getPosition();
-
-        int i, j;
-        for(i = -1; i <= 1; i++){
-
-            for(j = -1; j <= 1; j++){
-
-                if(i!=0 || j!=0) {
-                    int a = position.x + i;
-                    int b = position.y + j;
-                    if (a >= 0 && a <= 5 && b >= 0 && b <= 5) {
-                        int newSquareValue = board.fullMap[position.x + i][position.y + j].getValue();
-                        if (newSquareValue == 0) {
-                            Square square = board.fullMap[position.x + i][position.y + j];
-                            possibleBuilds.add(square);
-                        }
-                    }
-                }
-            }
-        }
+        ArrayList<Square> possibleBuilds = proximity(builder);
+        possibleBuilds = removeBuilderSquare(possibleBuilds);
+        possibleBuilds = removeDomeSquare(possibleBuilds);
 
         return possibleBuilds;
 
@@ -165,5 +126,70 @@ public class BasicRules implements Rules {
 
     @Override
     public void loseCondition() {
+    }
+
+    public ArrayList<Square> proximity(Builder builder) {
+
+        ArrayList<Square> proximity = new ArrayList<>();
+        Square position = builder.getPosition();
+
+        int i, j;
+        for (i = -1; i <= 1; i++) {
+
+            for (j = -1; j <= 1; j++) {
+
+                if (i != 0 || j != 0) {
+                    
+                    int a = position.x + i;
+                    int b = position.y + j;
+                    
+                    if (a >= 0 && a <= 5 && b >= 0 && b <= 5) {
+
+                        Square square = board.fullMap[a][b];
+                        proximity.add(square);
+
+
+                    }
+                }
+            }
+        }
+        
+        return proximity;
+        
+    }
+    
+    public ArrayList<Square> removeBuilderSquare(ArrayList<Square> proximity) {
+
+        for(int i = 0; i < proximity.size(); i++) {
+            if (proximity.get(i).getValue() == 1) {
+                proximity.remove(i);
+            }
+        }
+
+            return proximity;
+    }
+
+    public ArrayList<Square> removeDomeSquare(ArrayList<Square> proximity) {
+
+        for (int i = 0; i < proximity.size(); i++) {
+            if (proximity.get(i).getValue() == 2) {
+                proximity.remove(i);
+            }
+        }
+        return proximity;
+    }
+
+    public ArrayList<Square> removeTooHighPlaces(ArrayList<Square> proximity, Builder builder) {
+        Square position = builder.getPosition();
+        int playerHeight = position.getLevel();
+
+        for (int i = 0; i < proximity.size(); i++) {
+
+            int otherHeight = proximity.get(i).getLevel();
+            if (otherHeight - playerHeight >= maxHeight) {
+                proximity.remove(i);
+            }
+        }
+        return proximity;
     }
 }
--- src/main/java/it/polimi/ingsw/Model/Rules.java
+++ src/main/java/it/polimi/ingsw/Model/Rules.java
@@ -7,6 +7,10 @@ public interface Rules {
     void building(int x, int y);
     ArrayList<Square> getPossibleMoves(Builder builder);
     ArrayList<Square> getBuildingRange(Builder builder);
+    ArrayList<Square> proximity(Builder builder);
+    ArrayList<Square> removeBuilderSquare(ArrayList<Square> proximity);
+    ArrayList<Square> removeDomeSquare(ArrayList<Square> proximity);
+    ArrayList<Square> removeTooHighPlaces(ArrayList<Square> proximity, Builder builder);
     void winCondition(Builder builder, int x ,int y);
     void loseCondition();
 }
