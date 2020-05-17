7be1cc396dd05274f901d56f59d137764aa1b6e4
leona <leonardo.dezi@mail.polimi.it>
2020-05-17 18:31:43 +0200

    Parser

--- src/main/java/it/polimi/ingsw/Parser/ParserManager.java
+++ src/main/java/it/polimi/ingsw/Parser/ParserManager.java
@@ -8,10 +8,7 @@ import it.polimi.ingsw.Server.Model.Card;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.Reader;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.List;
 
 public class ParserManager {
 
@@ -26,12 +23,7 @@ public class ParserManager {
         Gson gson = new GsonBuilder().serializeNulls().create();
 
         try (Reader reader = new FileReader("Cards.json")) {
-
-            // Convert JSON File to Java Object
             Card card = gson.fromJson(reader, Card.class);
-
-            // print staff
-            System.out.println(card);
 
         } catch (IOException e) {
             e.printStackTrace();
